9577951a914e0f71c40dbb14479564bdf71a229a
evarest _okorowu_igwe <118117754+evarestigwe@users.noreply.github.com>
2023-02-07 23:52:23 +0100

    Update who.sh

--- who.sh
+++ who.sh
@@ -1,1 +1,2 @@
  love what am doing right now. God help me......how far with ujuaku...itshe actually learning ....how is madam measuring her progress; 
+please let see wether she can be seriousw....the year have just began'
