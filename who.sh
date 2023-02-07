a94dc22144e06c6defdd41d8e9a494f8f54d8460
2014git <evarestigwe@gmail.com>
2023-02-07 23:57:21 +0100

    check out

--- who.sh
+++ who.sh
@@ -1,1 +1,2 @@
- love what am doing right now. God help me......how far with ujuaku...itshe actually learning ....how is madam measuring her progress; 
+ love what am doing right now. God help me......how far with ujuaku...itshe actually learning ....how is madam measuring her progress.. pls see how you can put a check on her progress. thanks
+
